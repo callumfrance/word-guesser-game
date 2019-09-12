0df1a4d15a005f8527112da4fbb7abc322602da5
callumfrance <france.callum@gmail.com>
2019-09-12 19:11:30 +0800

    split classes into files; dedicated words folder
    Classes have their own py file.
    
    Now the words are in their own separate folder.

--- letter_game.py
+++ letter_game.py
@@ -1,127 +1,6 @@
-from random import shuffle, randrange
 import sys
-
-words = list() # Global variable that contains all of the words
-
-class GameStats:
-    """Class that contains game states and manages the game state
-    """
-
-    def __init__(self, in_game_board=None):
-        self.game_board = None
-        self.board_cnt = 0
-        self.correct = 0
-        if (in_game_board):
-            self.set_game_board(in_game_board)
-        else:
-            self.set_game_board(WordBoard())
-
-
-    def set_game_board(self, in_game_board):
-        self.game_board = in_game_board
-
-
-    def next_game_board(self):
-        self.game_board = WordBoard()
-        self.board_cnt = self.board_cnt + 1
-
-
-    def process_correct(self):
-        print("Correct")
-        self.correct = self.correct + 1
-
-
-    def print_stats(self):
-        print("Correct guesses: " + str(self.correct))
-        print("Boards generated: " + str(self.board_cnt))
-
-
-    def play_game(self):
-        # This is the 'view' of the game to the console for the player
-        will_exit = False
-        while(not will_exit):
-            self.game_board.print_board()
-            user_in = input('\n>')
-            if ((user_in == '--ans') or (user_in == '--help')):
-                print(self.game_board.ans_word)
-                self.next_game_board()
-            elif (self.game_board.check_answer(user_in)):
-                self.process_correct()
-                self.next_game_board()
-            elif (user_in == '--exit'):
-                print('Exiting...')
-                self.print_stats()
-                will_exit = True
-            else:
-                print("Not correct, try again")
-
-
-
-class WordBoard:
-    """Class that contains the word board and the correct word.
-    """
-
-    def __init__(self, in_word=None):
-        # Take or generate a word, and also create a randomized copy too
-        self.ans_word = None
-        self.board = list()
-        if (in_word):
-            self.set_word(in_word)
-        else:
-            self.generate_random_word()
-
-
-    def set_word(self, in_word):
-        # The answer word's mutator - also scrambles too
-        if (len(in_word) == 9):
-            self.ans_word = in_word
-            self._tile_scrambler()
-
-
-    def _tile_scrambler(self):
-        # Private function to scramble the answer word
-        temp_copy = list(self.ans_word)
-        shuffle(temp_copy)
-        self.board = ''.join(temp_copy)
-
-
-    def check_answer(self, to_check):
-        # Function that will check if a guess matches the answer word
-        is_correct = False
-        if (to_check == self.ans_word):
-            is_correct = True
-        return is_correct
-
-
-    def generate_random_word(self):
-        # This will pick a random word with `randrange` from the words.txt file
-        temp = words[randrange(0, len(words)-1)]
-        self.set_word(temp)
-
-
-    def print_board(self):
-        # This is how the board is represented in the console
-        print('+-------+', end='')
-        for i, n in enumerate(self.board):
-            if (i % 3 == 0):
-                print()
-                print('|', end='')
-            print(' ' + n, end='')
-            if (i % 3 == 2):
-                print(' |', end='')
-        print()
-        print('+-------+')
-
-
-
-def import_words(fileName):
-    # Import the 9-letter words into the program's memory from the word list
-    with open(fileName) as fo:
-        for line in fo:
-            temp = line.rstrip()
-            if(len(temp) == 9):
-                words.append(temp)
-
+from game_stats import GameStats
+from word_board import WordBoard
 
 
 if __name__ == '__main__':
@@ -130,10 +9,10 @@ if __name__ == '__main__':
         fileName = sys.argv[1]
         print("Using " + fileName)
     else:
-        fileName = 'popular9.txt'
-        print("Using popular.txt")
-    import_words(fileName)
-    wb = WordBoard()
+        # fileName = 'wordlists/popular9.txt'
+        fileName = 'wordlists/popular9.txt'
+        print("Using wordlists/popular9.txt")
+    wb = WordBoard(fileName)
     gb = GameStats(wb)
     print("Starting")
     gb.play_game()
---
+++ word_board.py
@@ -0,0 +1,73 @@
+from random import shuffle, randrange
+
+class WordBoard:
+    """Class that contains the word board and the correct word.
+    """
+
+    def __init__(self, in_words_file_name=None):
+        # Take or generate a word, and also create a randomized copy too
+        self.ans_word = None
+        self.board = list()
+        self.words = list()
+        print("in word file name : " + str(in_words_file_name))
+        if (in_words_file_name):
+            self.load_words(in_words_file_name)
+        else:
+            self.load_words()
+        self.generate_random_word()
+        print("WordBoard: " + str(len(self.words)) + "\t" + self.ans_word)
+
+
+    def set_word(self, in_word):
+        # The answer word's mutator - also scrambles too
+        if (len(in_word) == 9):
+            self.ans_word = in_word
+            self._tile_scrambler()
+
+
+    def _tile_scrambler(self):
+        # Private function to scramble the answer word
+        temp_copy = list(self.ans_word)
+        shuffle(temp_copy)
+        self.board = ''.join(temp_copy)
+
+
+    def check_answer(self, to_check):
+        # Function that will check if a guess matches the answer word
+        is_correct = False
+        if (to_check == self.ans_word):
+            is_correct = True
+        return is_correct
+
+
+    def load_words(self, in_words_file_name=None):
+        # Loads words into object from filename, either provided or defaulted
+        if (not in_words_file_name):
+            in_words_file_name = 'wordlists/popular9.txt'
+        with open(in_words_file_name) as fo:
+            print("Opening " + in_words_file_name)
+            for line in fo:
+                temp = line.rstrip()
+                if(len(temp) == 9):
+                    self.words.append(temp)
+
+
+    def generate_random_word(self):
+        # This will pick a random word with `randrange` from the words.txt file
+        temp = self.words[randrange(0, len(self.words)-1)]
+        self.set_word(temp)
+
+
+    def print_board(self):
+        # This is how the board is represented in the output
+        board_str = "+-------+"
+        for i, n in enumerate(self.board):
+            if (i % 3 == 0):
+                board_str = board_str + "\n|"
+            board_str = board_str + " " + n
+            if (i % 3 == 2):
+                board_str = board_str + " |"
+        board_str = board_str + "\n+-------+"
+        return(board_str)
+
+
